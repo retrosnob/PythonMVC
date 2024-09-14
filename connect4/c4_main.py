540e7947086b15af1e26686905d641ecae483da2
Justin Robertson <jusrobertson@yahoo.co.uk>
2024-09-14 21:31:02 +0100

    Removed command line interface

--- connect4/c4_main.py
+++ connect4/c4_main.py
@@ -15,16 +15,3 @@ if __name__ == "__main__":
 
     while controller.running:
         controller.process_input()
-
-
-
-    # model = c4_model.C4_Model()
-    # while not model.getstatus()["GAME OVER"]:
-    #     column = input('Enter column: ')
-    #     model.makemove(int(column))
-    #     model.print()
-    # winner = model.getstatus()["WINNER"]
-    # if winner:
-    #     print("Winner: Player {}".format(winner))
-    # else:
-    #     print("Draw")
